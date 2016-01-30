572a8a211a2617c018517f57eb53790469adc830
Alex Dron <dronfree@gmail.com>
2016-01-30 14:02:55 +0300

    dumb routes /box and /mails

--- src/mbm/mbm.go
+++ src/mbm/mbm.go
@@ -10,5 +10,11 @@ func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome from Mail boxes manager")
 	})
+	http.HandleFunc("/box", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "box")
+	})
+	http.HandleFunc("/mails", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "mails")
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
