ab62575006f6f4d41528f610c14ba79e37e9b64a
Alex Dron <dronfree@gmail.com>
2016-02-05 16:47:49 +0300

    #13: first completed dumb implementation, no Go style yet

--- src/mbm/mbm.go
+++ src/mbm/mbm.go
@@ -3,7 +3,6 @@ import (
 	"fmt"
 	"net/http"
 	"log"
-	"html"
 	"flag"
 	"os"
 	"bufio"
@@ -11,27 +10,68 @@ import (
 	"strconv"
 	"os/exec"
 	"strings"
+	"time"
+	"encoding/json"
+	"io/ioutil"
 )
 
 type Params struct {
 	port      int
 	mailboxes string
-	rentfor   int
+	rentfor   int64
+	boxpath   string
+	checkexpire time.Duration
+	freecapacity int
+	makefreeavailable time.Duration
+}
+type BusyBox struct {
+	box    string
+	expireAt time.Time
+}
+type AvailableBox struct {
+	Box    string
+	Sessid string
+}
+type AllBoxBusy struct {
+	Error string
 }
 var params Params
+var (
+	available []string
+	free      []string
+	busy      = make(map[string]BusyBox)
+)
 
 func init() {
+
 	flag.IntVar(&params.port, "port", 8080, "Port to start app on")
+	flag.DurationVar(&params.checkexpire, "checkexpire", 5*time.Second, "How often to perform check expire boxes in seconds")
+	flag.IntVar(&params.freecapacity, "freecapacity", 5, "Max number of expired boxes to return to queue")
+	flag.DurationVar(&params.makefreeavailable, "makefreeavailable", 5*time.Second, "How often to perform makefreeavailable")
 	flag.StringVar(&params.mailboxes, "mailboxes", "vmailbox", "Postfix virtual map file")
-	flag.IntVar(&params.rentfor, "rentfor", 3600, "Mailbox rent time in seconds")
+	flag.StringVar(&params.boxpath, "boxpath", "/var/www/boxes", "Path to directory with stored boxes")
+	flag.Int64Var(&params.rentfor, "rentfor", 3600, "Mailbox rent time in seconds")
+}
+
+func SessId() string {
+	var (
+		uuid []byte
+		err error
+	)
+	if uuid, err = exec.Command("uuidgen").Output(); err != nil {
+		log.Fatal(err)
+	}
+	return strings.Trim(string(uuid), "\n")
+}
+
+func boxFile(box string) string {
+	return params.boxpath + "/" + strings.Split(box, "@")[0]
 }
 
 func main() {
 	var (
 		err    error
 		inFile *os.File
-		uuid   []byte
-		sessId string
 	)
 	flag.Parse()
 	if inFile, err = os.Open(params.mailboxes); err != nil {
@@ -49,25 +89,96 @@ func main() {
 		} else if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Print(line)
+		lineArr := strings.Split(line, " ")
+		available = append(available, lineArr[0])
 	}
-
-	fmt.Println(params.mailboxes)
-	fmt.Println(params.port)
-	fmt.Println(params.rentfor)
-	uuid, err = exec.Command("uuidgen").Output()
-	sessId = strings.Trim(string(uuid), "\n")
-	fmt.Println(sessId)
-
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome from Mail boxes manager")
 	})
 	http.HandleFunc("/box", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "box: path %q, query %q", html.EscapeString(r.URL.Path), html.EscapeString(r.URL.RawQuery))
+		var box string
+		var err error
+		var js []byte
+		if len(available) == 0 {
+			if js, err = json.Marshal(AllBoxBusy{"allbusy"}); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		} else {
+			box, available = available[0], available[1:len(available)]
+			sessId := SessId()
+			busy[sessId] = BusyBox{box, time.Now().Add(time.Duration(1e9 * params.rentfor))}
+
+			availableBox := AvailableBox{box, sessId}
+			if js, err = json.Marshal(availableBox); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			if err = os.Remove(boxFile(box)); err != nil {
+				log.Println(err)
+			}
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js);
 	})
 	http.HandleFunc("/mails", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "mails: path %q, query %q", html.EscapeString(r.URL.Path), html.EscapeString(r.URL.RawQuery))
+		var boxArr, sessidArr []string
+		var box, sessid string
+		var busyRow BusyBox
+		var ok bool
+		var err    error
+		var inFile *os.File
+		var content []byte
+
+		if boxArr, ok = r.URL.Query()["box"]; !ok {
+			log.Println("box paramether not set")
+			return
+		}
+		if sessidArr, ok = r.URL.Query()["sessid"]; !ok {
+			log.Println("sessid paramether not set")
+			return
+		}
+		box = boxArr[0]
+		sessid = sessidArr[0]
+
+		if busyRow, ok = busy[sessid]; !ok || busyRow.box != box {
+			log.Println("sessid of box not match")
+			return
+		}
+		if inFile, err = os.Open(boxFile(box)); err != nil {
+			log.Println(err)
+			return
+		}
+		defer inFile.Close()
+		if content, err = ioutil.ReadAll(bufio.NewReader(inFile)); err != nil {
+			log.Println(err)
+			return
+		}
+		w.Write(content);
 	})
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(params.port), nil))
+	go expireBox()
+	go makeFreeAvailable()
+	http.ListenAndServe(":"+strconv.Itoa(params.port), nil)
+}
+
+func expireBox() {
+	for {
+		for i, k := range busy {
+			if time.Now().After(k.expireAt)  {
+				delete(busy, i)
+				free = append(free, k.box)
+				log.Println(k.box + " expired")
+			}
+		}
+		time.Sleep(params.checkexpire)
+	}
+}
+
+func makeFreeAvailable() {
+	for {
+		if len(free) > params.freecapacity {
+			available = append(available, free[0:params.freecapacity-1]...)
+			free = free[params.freecapacity:]
+		}
+		time.Sleep(params.makefreeavailable)
+	}
 }
